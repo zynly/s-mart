8e78e58726c8c4a16321f68ef0fa93c1658681a4
Mahin Utsman Nawawi <mahinutsmannawawi20@gmail.com>
2026-08-13 10:00:38 +0700

    fix(database): isolate pgsql charset to DB_PGSQL_CHARSET to prevent client_encoding utf8mb4 fatal error

--- config/database.php
+++ config/database.php
@@ -141,7 +141,7 @@ return [
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'charset' => env('DB_PGSQL_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
