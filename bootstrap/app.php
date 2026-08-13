8c961c4caf497397b4b4db66d17e53521e4e6c6f
Mahin Utsman Nawawi <mahinutsmannawawi20@gmail.com>
2026-08-13 10:14:38 +0700

    fix(auth): add trustProxies(at: '*') to prevent 302 login redirect loops behind reverse proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,8 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->validateCsrfTokens(except: [
             'midtrans/notification',
             'api/v1/callback/pakasir',
